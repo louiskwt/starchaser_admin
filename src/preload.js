1f910c5f0a76397f263a747433c27825a236c09b
Louis Tsang <kawingt2@gmail.com>
2023-02-16 00:26:19 +0800

    added pending task table

--- src/preload.js
+++ src/preload.js
@@ -5,6 +5,25 @@ const { turncate } = require("./utils/");
 
 window.addEventListener("DOMContentLoaded", () => {
   const taskList = document.getElementById("task-table");
+  const pendingTaskList = document.getElementById("pending-task-table");
+
+  const taskRef = db.collection("tasks");
+  const pendingTasks = taskRef.where("status", "==", "pending").get();
+
+  pendingTasks.then((querySnapshot) => {
+    if (!querySnapshot.empty) {
+      const tasks = querySnapshot.docs.map((doc) => {
+        const taskElemet = document.createElement("tr");
+        const task = doc.data();
+        taskElemet.innerHTML = `
+          <td>${turncate(task.name, 20)}</td>
+          <td><a href='${task.url}' target="_blank" >Download</a></td>
+          <td>${task.status}</td>
+        `;
+        pendingTaskList.appendChild(taskElemet);
+      });
+    }
+  });
 
   db.collection("tasks").onSnapshot((querySnapshot) => {
     querySnapshot.forEach((doc) => {
